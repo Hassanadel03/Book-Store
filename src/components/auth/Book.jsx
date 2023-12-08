9e9c42581414b0d40b6038b90e86cdce3cc07609
Hassanadel03 <hassanadel20003@gmail.com>
2023-12-08 17:42:22 +0200

    make download button

--- src/components/auth/Book.jsx
+++ src/components/auth/Book.jsx
@@ -1,4 +1,4 @@
-import './UserCSS.css'
+import './UserCSS.css';
 
 function Book({ book }) {
     // Check if 'book' is defined before accessing its properties
@@ -6,14 +6,40 @@ function Book({ book }) {
         return null; // or handle the case when 'book' is not defined
     }
 
-    const { bookName, pdfName, info } = book;
+    const { bookName, pdfName, author, info } = book;
+
+    const handleDownload = () => {
+        // Assuming pdfName is the correct path to the PDF file on your server
+        const pdfUrl = `/auth/pdf/${pdfName}`;
+
+        // Create a link element
+        const link = document.createElement('a');
+
+        // Set the href attribute to the PDF URL
+        link.href = pdfUrl;
+
+        // Specify that the link should be downloaded when clicked
+        link.setAttribute('download', pdfName);
+
+        // Append the link to the document
+        document.body.appendChild(link);
+
+        // Trigger a click on the link to start the download
+        link.click();
+
+        // Remove the link from the document
+        document.body.removeChild(link);
+    };
 
     return (
         <div className="card">
             <h3>{bookName}</h3>
-            <p>PDF: {pdfName}</p>
+            <p>author: {author}</p>
+
             <p>Info: {info}</p>
 
+            {/* Add a button for downloading */}
+            <button onClick={handleDownload}>Download PDF</button>
         </div>
     );
 }
