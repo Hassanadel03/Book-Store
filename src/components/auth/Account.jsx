1dd55e391b3d412dee98faf8a383baa3f7bd3df4
Hassanadel03 <hassanadel20003@gmail.com>
2023-12-09 06:14:22 +0200

    update line

--- src/components/auth/Account.jsx
+++ src/components/auth/Account.jsx
@@ -1,18 +1,18 @@
 import { Box, Typography } from '@mui/material';
 const Account = () => {
-    return (  <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          height: '100vh', // Set height to 100% of viewport height
-        }}
-      >
-        <Typography variant="h5">
-          You have not signed in yet. Please sign in to view your cart.
-        </Typography>
-      </Box> );
+  return (<Box
+    sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '100vh', // Set height to 100% of viewport height
+    }}
+  >
+    <Typography variant="h5">
+      You have not signed in yet. Please sign in to view your Account.
+    </Typography>
+  </Box>);
 }
- 
-export default Account ;
+
+export default Account;
